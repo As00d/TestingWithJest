b533c9d7591fa2c3efb611995a1afa31a5557449
Ankita Sood <ankisood@amazon.com>
2024-06-09 13:03:05 +0530

    Matchers : toThrow

--- sum.js
+++ sum.js
@@ -1,7 +1,13 @@
-const sumOfTwoNumber = function (num1, num2) {
-  return num1 + num2;
-};
+// const sumOfTwoNumber = function (num1, num2) {
+//   return num1 + num2;
+// };
 
-module.exports = sumOfTwoNumber;
+// module.exports = sumOfTwoNumber;
 
+const myFunction = function(invalidInput) {
+    if (typeof invalidInput !== "number") {
+      throw new Error("Invalid input");
+    }
+}
+module.exports = myFunction;
 // Basic summary of what is written 1. Created a very basic function called sum which basically add two numbers and then line 5 basically allow us to export this created method anywhere. So it can be exported and be used by test file.
--- sum.test.js
+++ sum.test.js
@@ -1,34 +1,41 @@
-const sumOfTwoNumber = require('./sum');
+// const sumOfTwoNumber = require('./sum');
+const myFunction = require("./sum");
 
-test('Add 2 + 2 equals 4', ()=>{
-    expect(sumOfTwoNumber(2,2)).toBe(4);
-})
+// test('Add 2 + 2 equals 4', ()=>{
+//     expect(sumOfTwoNumber(2,2)).toBe(4);
+// })
 
 // toBe -> when its primitive data type
-test('Learn about to be',()=>{
-    expect(4+2).toBe(6);
-})
+test("Learn about to be", () => {
+  expect(4 + 2).toBe(6);
+});
 
 // toEqual -> when its not primitive datatype
-test('learn about equalTo', ()=>{
-    const data = {
-        one:1
-    }
-    data.two = 2;
-    expect(data).toEqual({two:2,one:1})
-})
+test("learn about equalTo", () => {
+  const data = {
+    one: 1,
+  };
+  data.two = 2;
+  expect(data).toEqual({ two: 2, one: 1 });
+});
 
 // toBeFalsy
-test('Learn about toBeFalsy',()=>{
-    const name='';
-    expect(name).toBeFalsy();
-})
+test("Learn about toBeFalsy", () => {
+  const name = "";
+  expect(name).toBeFalsy();
+});
 // toBeTruthy
-test('Learn about Truthy',()=>{
-    const name = "anki";
-    expect(name).toBeTruthy();
-})
+test("Learn about Truthy", () => {
+  const name = "anki";
+  expect(name).toBeTruthy();
+});
 
+// toThrow
+test("Check if method throws an error", () => {
+  expect(() => {
+    myFunction('test');
+  }).toThrow();
+});
 // As we saw where actual method is written we wrote an export statement which allowed us to export the sum method. Now in order to use that method here we have to import it first which we do by using require.
 // In test we have two parameter test('description', function - mostly arrow function is used)
 // There is something called as matchers for eg toBe, equal truthy falsy etc
@@ -36,3 +43,4 @@ test('Learn about Truthy',()=>{
 // 2. toEqual is used when we are comparing the values of two objects or arrays
 // 3. toBeFalsy : this is used when we expect the value to be null, undefined, '', false, 0, NAN
 // 4. toBeTruthy : Any thing except what is falsy is truthy.
+// One more thing we have is error handling in matchers for that we have toThrow
